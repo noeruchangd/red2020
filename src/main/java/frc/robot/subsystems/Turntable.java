201766bf9c20542dbf46663d2fa722529889c0c9
Yoda8778 <haiduongnguyen32@gmail.com>
2021-01-05 16:18:09 +0700

    buoi

--- src/main/java/frc/robot/subsystems/Turntable.java
+++ src/main/java/frc/robot/subsystems/Turntable.java
@@ -7,8 +7,6 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
